cea90d00a9b2db4ca0ca1e73dbf1700fd27089b9
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-01-08 00:11:09 +0200

    added txt files for 2 different levels

---
+++ Levels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProjectSudoku
+{
+    class Levels
+    {
+
+        string PathToEasyEmpty1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles.txt";
+        string PathToEasySolution1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles.txt";
+
+        string PathToMediumEmpty2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles.txt";
+        string PathToMediumSolution2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles.txt";
+
+    }
+}
