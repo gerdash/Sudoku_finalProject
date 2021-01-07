52ac6f54ba5e7ee0b3c6aae61768fa39736c1a74
Rozenbergas <63879482+Rozenbergas@users.noreply.github.com>
2021-01-08 00:55:39 +0200

    2 grid options (solved and empty) for 2 levels

--- Levels.cs
+++ Levels.cs
@@ -6,12 +6,14 @@ namespace FinalProjectSudoku
 {
     class Levels
     {
-
-        string PathToEasyEmpty1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles.txt";
-        string PathToEasySolution1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles.txt";
-
-        string PathToMediumEmpty2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles.txt";
-        string PathToMediumSolution2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles.txt";
-
+        //for now only 2 levels.To check how ths will work. This is work in progress...
+        string PathToEasyEmpty1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles\EasyEmpty1.txt";
+        string PathToEasySolution1_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles\EasySolution1.txt";
+        string PathToEasyEmpty1_2 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles\EasyEmpty2.txt";
+        string PathToEasySolution1_2 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\easyleveltxtfiles\EasySolution2.txt";
+        string PathToMediumEmpty2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles\mediumlevelempty1.txt";
+        string PathToMediumSolution2_1 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles\mediumlevelsolution1.txt";
+        string PathToMediumEmpty2_2 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles\mediumlevelempty2.txt";
+        string PathToMediumSolution2_2 = @"C:\Users\Sandra Aunina\source\repos\Sudoku_finalProject\mediumleveltxtfiles\mediumlevelsolution2.txt";
     }
 }
